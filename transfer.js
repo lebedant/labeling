f12d958d92d4a4733953c278e60bcaea51479bf6
Antonina Lebedeva <antonina.lebedeva@eman.cz>
2017-12-08 23:48:47 +0100

    fix in lib

--- transfer.js
+++ transfer.js
@@ -1,33 +1,34 @@
-class Transfer {
-    static registerParticipant() {
+"use strict";
+var Transfer = (function () {
+    function Transfer() {
+    }
+
+    Transfer.registerParticipant = function () {
         // do the registration
         // ajax request
         var promise = $.ajax({
             context: this,
             method: "POST",
-            url: "http://localhost:3030/api/v1/register_participant.json",
+            url: Transfer.host + "/api/v1/register_participant.json",
             dataType: "json",
         });
-
-        promise.done(function(response) {
+        promise.done(function (response) {
             // save participant id
             localStorage.setItem("participantId", response.internal_id);
-            console.info(`Participant ID: ${response.internal_id}`);
+            console.info("Participant ID: " + response.internal_id);
             console.info('Successfull registration.');
         });
-
-        promise.fail(function(xhr, status, error) {
+        promise.fail(function (xhr, status, error) {
             console.error('Error registration:' + xhr.responseJSON.message);
         });
-
         return promise;
-    }
+    };
 
-    static sendData(partToken, params) {
+    Transfer.sendData = function (partToken, params) {
         // ajax request
         var promise = $.ajax({
             method: "POST",
-            url: "http://localhost:3030/api/v1/save_data.json",
+            url: Transfer.host + "/api/v1/save_data.json",
             contentType: "application/json",
             data: JSON.stringify({
                 part_id: partToken,
@@ -36,15 +37,15 @@ class Transfer {
             }),
             dataType: "json"
         });
-
-        promise.done(function(response) {
+        promise.done(function (response) {
             console.info('Successfull data transfer.');
         });
-
-        promise.fail(function(xhr, status, error) {
+        promise.fail(function (xhr, status, error) {
             console.error('Error send data: ' + error + " - " + xhr.responseJSON.message);
         });
-
         return promise;
-    }
-}
+    };
+    // Transfer.host = 'https://experiments-tool.herokuapp.com';
+    Transfer.host = 'http://localhost:3030';
+    return Transfer;
+}());
